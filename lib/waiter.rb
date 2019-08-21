5495dc65c6fbf3e95838195c445bbaee98d0a1c0
Cloe Kouadjo <armelfli50@gmail.com>
2019-08-21 06:35:41 +0000

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -21,8 +21,8 @@ class Waiter
   
   def best_tipper 
     array_of_tips = []
-    self.customers.each{|el| array_of_tips << el.tip }
-    self.customers.find{|el| el if el.tip == array_of_tips.max}
+    self.meals.each{|el| array_of_tips << el.tip }
+    self.meals.find{|el| el.customer if el.tip == array_of_tips.max}
     
   end
 end
