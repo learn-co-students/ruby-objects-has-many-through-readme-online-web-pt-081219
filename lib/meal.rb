953581fccc5d9a21530e7328ca3b0c22f8aab2e6
Dylan Crawshaw <dcrawsh@gmail.com>
2019-09-19 21:15:52 +0000

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,18 @@
 class Meal
+attr_accessor :waiter, :customer, :total, :tip 
+@@all = Array.new 
+
+def initialize(waiter, customer, total, tip)
+@waiter = waiter 
+@customer = customer 
+@tip = tip 
+Meal.all << self 
+end 
+
+def self.all
+@@all 
+end 
+
+
 
 end
