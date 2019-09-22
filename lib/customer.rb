a819e1327505c3e900aacec88c0a9a765c0c78a9
James Mobus <mobusj@live.com>
2019-09-22 18:39:41 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,28 @@
 class Customer
+  attr_reader :name, :age, :meals, :waiters
   
+  @@all = []
+  
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @meals = []
+    @waiters = []
+    
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_meal(waiter, total, tip)
+    meal = Meal.new(waiter, self, total, tip)
+    
+    waiter.check_tip(self, tip)
+    waiter.meals.push(meal)
+    
+    @waiters.push(waiter)
+    @meals.push(meal)
+  end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,35 @@
 class Waiter
-
+  attr_reader :name, :years_of_experience, :meals, :best_tip, :best_tipper
+  
+  @@all = []
+  
+  def initialize(name, years_of_experience)
+    @name = name
+    @years_of_experience = years_of_experience
+    @meals = []
+    @best_tip = 0
+    
+    @@all << self 
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_meal(customer, total, tip)
+    check_tip(customer, tip)
+    
+    @meals.push(Meal.new(self, customer, total, tip))
+  end 
+  
+  def check_tip(customer, tip)
+    if tip > @best_tip 
+      @best_tip = tip
+      @best_tipper = customer
+    end
+  end
+  
+  def meals
+    @meals
+  end
 end
